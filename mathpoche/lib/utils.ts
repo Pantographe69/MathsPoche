c93c6c6caa330207f7dfec39f66ffd781e30958b
Pantographe69 <89343872+Pantographe69@users.noreply.github.com>
2026-04-21 23:22:40 +0200

    Delete mathpoche directory

--- mathpoche/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-/** Combines clsx + tailwind-merge for conditional className composition. */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
